d6a114d0c175798e71b006f2870ce3d6604ec257
Eric S <eric2620371@gmail.com>
2024-01-24 21:26:45 +0000

    the script now tells how much time has passed, can be stopped by pressing right control

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+from pynput import keyboard
+import time
+
+time_counter = 0.0
+stopProgram = False
+
+
+def check_for_end(key):
+    global time_counter, stopProgram
+    if key == keyboard.Key.ctrl_r:  # if the right ctrl key is pressed then the loop will break
+        stopProgram = True
+
+
 def main():
-    pass
+    global time_counter, stopProgram
+
+    # start the event listener in a new thread
+    listener = keyboard.Listener(on_press=check_for_end, on_release=None)
+    listener.start()
+
+    while stopProgram is False:
+        time.sleep(1)
+        time_counter += 1.0  # adds 1 to the time counter every second
+        print(time_counter)
 
 
 if __name__ == '__main__':
     main()
-

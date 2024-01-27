2e67a46852dfdbbaaf8ac3daa3ac8de0e49cbf26
JOELTORCATO <a12022@ae4outubro.pt>
2024-01-27 16:07:52 +0000

    UPDATE!
    I UPDATE THE WEB FOLDER.
    
    Co-Authored-By: Eric S. <118106151+epicsimoes@users.noreply.github.com>

--- WEB SITE/script.js
+++ WEB SITE/script.js
@@ -1,3 +1,4 @@
+// script.js
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxHdxYtnFHRHdAbjLiUQ0d_IBgC0GWgEVxa5he79_kWmk_g6k1LSSjzJga26p2DUqfLRg/exec'
 const form = document.getElementById('submit-to-google-sheet');
 

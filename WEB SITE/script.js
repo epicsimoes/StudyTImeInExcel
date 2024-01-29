e933632fefc6c39fed0051932c43f473c73db422
JOELTORCATO <a12022@ae4outubro.pt>
2024-01-29 16:03:18 +0000

    ORGANIZATION
    i organized the html, css and javascript documents in the web site folder.

--- WEB SITE/script.js
+++ WEB SITE/script.js
@@ -1,8 +1,9 @@
 // script.js
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxHdxYtnFHRHdAbjLiUQ0d_IBgC0GWgEVxa5he79_kWmk_g6k1LSSjzJga26p2DUqfLRg/exec'
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwI0V_hVBXLCcU4dt_rRVXVS7u6n97E7eo0_AlyizkL4Mc2asNoCJoKh4_pOE-7UscL5w/exec';
 const form = document.getElementById('submit-to-google-sheet');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then(response => console.log('Success!', response))
@@ -10,12 +11,12 @@ form.addEventListener('submit', e => {
 });
 
 function validateForm() {
-  var name = document.getElementById('name').value;
-  var password = document.getElementById('password').value;
+  const name = document.getElementById('name').value;
+  const password = document.getElementById('password').value;
 
   if (name.trim() === '' || password.trim() === '') {
-    alert('please fill in all required fields (name and password).');
-    return false;
+    alert('Please fill in all required fields (name and password).');
+    return true;
   } else {
     window.location.href = 'exel.html';
     return false;
